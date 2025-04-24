9aa75baa104eae84076dacdbaf5eb9bb3319316c
Jacob Kim <jkim57@emich.edu>
2025-04-24 03:42:00 -0400

    dark mode and added bopomofo page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import BopomofoCharacters from './BopomofoCharacters';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        {/* Navigation */}
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/about">About</Link>
-        </nav>
+      <div className = "App-background">
+        <div className="App">
+          {/* Navigation */}
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/about">About</Link> | 
+            <Link to="/BopomofoCharacters">BopomofoCharacters</Link>
+          </nav>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </Router>
+          {/* Routes */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/BopomofoCharacters" element={<BopomofoCharacters />} />
+          </Routes>
+        </div>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/BopomofoCharacters.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function BopomofoCharacters() {
+    return (
+        <>
+    <h1>
+    Initials (聲母):
+    </h1>   
+    <h2>
+    ㄅ (b) ㄆ (p) ㄇ (m) ㄈ (f) ㄉ (d) ㄊ (t) ㄋ (n) ㄌ (l) ㄍ (g) ㄎ (k) ㄏ (h) ㄐ (j) ㄑ (q) ㄒ (x) ㄓ (zh) ㄔ (ch) ㄕ (sh) 
+    ㄖ (r) ㄗ (z) ㄘ (c) ㄙ (s)
+    </h2>
+
+    <h1>
+    Finals (韻母):
+    </h1>
+    <h2>
+    ㄧ (i) ㄨ (u) ㄩ (ü) ㄚ (a) ㄛ (o) ㄜ (e) ㄝ (ê) ㄞ (ai) ㄟ (ei) ㄠ (ao) ㄡ (ou) ㄢ (an) ㄣ (en) ㄤ (ang) ㄥ (eng) ㄦ (er)
+    </h2>
+
+    <h1>
+    Tone Marks (聲調):
+    </h1>
+    <h2>
+    ˉ (1st tone)
+    
+    ˊ (2nd tone)
+    
+    ˇ (3rd tone)
+    
+    ˋ (4th tone)
+    
+    ˙ (neutral tone)
+    </h2>
+    </>
+    );
+  }
+  
+  export default BopomofoCharacters;
