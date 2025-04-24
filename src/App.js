51175baba72758248bb58bb97e49f7638f0fcd9d
Jacob Kim <jkim57@emich.edu>
2025-04-24 03:06:37 -0400

    added router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <button className="btn btn-primary">Styled Button</button>
-  </div>
+    <Router>
+      <div className="App">
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Welcome to Home Page</h2>;
+}
+
+function About() {
+  return <h2>This is the About Page</h2>;
 }
 
 export default App;
